2ca6c3a6248f971e46e558ab6e2b60134442ce39
nishil <nishyl@gmail.com>
2017-05-07 04:17:01 +0530

    cleanup, comments

--- background.js
+++ background.js
@@ -4,11 +4,13 @@
 
 function logURL(requestDetails) {
   console.log("Loading: " + requestDetails.url);
+  // notify content script
   chrome.tabs.query({active:true,currentWindow:true},function(tabs){
         chrome.tabs.sendMessage(tabs[0].id,requestDetails);
     });
 }
 
+// wait for API request for restaurant list to complete
 chrome.webRequest.onCompleted.addListener(
   logURL,
   {urls: ["https://www.swiggy.com/api/restaurants/list*"]}
--- makemap.js
+++ makemap.js
@@ -1,15 +1,34 @@
 // content-script.js
 "use strict";
 
-chrome.runtime.onMessage.addListener(request => {
-  console.log(request.greeting);
-  console.log('reststaurant info loaded');
-  var $resItem = $('.restaurant-container')[0];
+var requestCount = 0;
+var $resItem;
+var mapIframeHtml = '\
+  <iframe width="100%" height="450" frameborder="0" style="border:0" \
+  src="https://www.google.com/maps/embed/v1/place?q=delhi&key=AIzaSyCEEHDMXaQK1ax-6EuR5fe9YMSHUIAytjY" \
+  allowfullscreen></iframe>';
+
+function showMap(){
+  // find first div with class 'restaurant container'
+  $resItem = $('.restaurant-container')[0];
   var mapDiv = document.createElement('div');
-  mapDiv.innerHTML = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=delhi&key=AIzaSyCEEHDMXaQK1ax-6EuR5fe9YMSHUIAytjY" allowfullscreen></iframe> '
+  mapDiv.innerHTML = mapIframeHtml;
+  // insert map into DOM
   $resItem.parentNode.parentNode.insertBefore(mapDiv, $resItem.parentNode);
-  console.log('done');
-  return Promise.resolve();
+}
+
+function updateMap(){
+}
+
+// Wait for message from background script
+chrome.runtime.onMessage.addListener(request => {
+  console.log(request);
+  // only create new map for first api call
+  if (requestCount===0){
+    showMap();
+  }else {
+    updateMap();
+  }
 });
 
 console.log('I am alive');
