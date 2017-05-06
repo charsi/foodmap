da437703ee9aefa1894cfbc6c292c29dac6a71b8
nishil <nishyl@gmail.com>
2017-05-07 00:01:10 +0530

    background request test

--- makemap.js
+++ makemap.js
@@ -4,20 +4,4 @@ console.log($resItem);
 var mapDiv = document.createElement('div');
 mapDiv.innerHTML = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=delhi&key=AIzaSyCEEHDMXaQK1ax-6EuR5fe9YMSHUIAytjY" allowfullscreen></iframe> '
 $resItem.parentNode.parentNode.insertBefore(mapDiv,$resItem.parentNode);
-console.log('done')
-
-
-var pattern = "https://mdn.mozillademos.org/*";
-
-function redirect(requestDetails) {
-  console.log("Redirecting: " + requestDetails.url);
-  // return {
-  //   redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif"
-  // };
-}
-
-browser.webRequest.onBeforeRequest.addListener(
-  redirect,
-  {urls:[pattern]},
-  ["blocking"]
-);
+console.log('done');
