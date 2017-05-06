b62ac06387ebeaf995ad47a6163450f4b7825818
nishil <nishyl@gmail.com>
2017-05-07 03:49:18 +0530

    wait for dom load before loading map

--- background.js
+++ background.js
@@ -1,8 +1,28 @@
-function logURL(requestDetails) {
-  console.log("Loading: " + requestDetails.url);
+"use strict";
+
+function onError(error) {
+  console.error(`Error: ${error}`);
 }
 
-browser.webRequest.onBeforeRequest.addListener(
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {greeting: "Hi from background script"}
+    ).catch(onError);
+  }
+}
+
+
+function logURL(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs).catch(onError);
+}
+
+chrome.webRequest.onCompleted.addListener(
   logURL,
-  {urls: ["https://*.swiggy.com/*"]}
+  {urls: ["https://www.swiggy.com/api/restaurants/list*"]}
 );
--- makemap.js
+++ makemap.js
@@ -1,7 +1,21 @@
+// content-script.js
+"use strict";
+
+browser.runtime.onMessage.addListener(request => {
+  console.log(request.greeting);
+  console.log('reststaurant info loaded');
+  var $resItem = $('.restaurant-container')[0];
+  var mapDiv = document.createElement('div');
+  mapDiv.innerHTML = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=delhi&key=AIzaSyCEEHDMXaQK1ax-6EuR5fe9YMSHUIAytjY" allowfullscreen></iframe> '
+  $resItem.parentNode.parentNode.insertBefore(mapDiv, $resItem.parentNode);
+  console.log('done');
+  return Promise.resolve();
+});
+
 console.log('I am alive');
-var $resItem = $('.restaurant-container')[0];
-console.log($resItem);
-var mapDiv = document.createElement('div');
-mapDiv.innerHTML = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=delhi&key=AIzaSyCEEHDMXaQK1ax-6EuR5fe9YMSHUIAytjY" allowfullscreen></iframe> '
-$resItem.parentNode.parentNode.insertBefore(mapDiv,$resItem.parentNode);
-console.log('done');
+
+
+      
+
+
+
